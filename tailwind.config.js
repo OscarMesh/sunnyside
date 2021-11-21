c27d59050ff03805e17a307a710bab498d9912f8
OscarMesh <93758969+OscarMesh@users.noreply.github.com>
2021-11-21 08:12:02 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:{
+                cyan:'hsl(167, 40%, 24%)',
+                Darkblue:'hsl(198, 62%, 26%)',
+                moderatecyan :'hsl(168, 34%, 41%)'
+        } 
+
+        
+      },
+
+      fontFamily:{
+        body: ['Fraunces']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
